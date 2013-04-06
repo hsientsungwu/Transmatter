80bff6b1308a64ac8c39da1dcc67d0f5a99a0895
HsienTsung Wu <hsientsungwu@HsienTsungs-MacBook-Pro.local>
2013-04-05 22:47:03 -0400

    Add AutoComplete Styles

--- index.php
+++ index.php
@@ -7,14 +7,14 @@ header("Content-Type: text/html; charset=utf-8");
 		<title>Steve's Autocomplete Dictionary</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" ></script>
-		<script type="text/javascript" src="javascripts/jquery-ui-1.10.2.custom.min.js"></script>
-		<link rel="stylesheet" href="css/ui-lightness/jquery-ui-1.10.2.custom.css">
-		<script type="text/javascript" src="javascripts/search2.js"></script>
+		<script type="text/javascript" src="/js/jqueryui/jquery-ui-1.10.2.custom.min.js"></script>
+		<script type="text/javascript" src="/js/application.js"></script>
+		<link rel="stylesheet" href="/css/jqueryui/ui-lightness/jquery-ui-1.10.2.custom.min.css">
 	</head>
 	<body>
 		<div class="search-bar-container">
 			<form id="id_searchform" >
-				<h4>Search word here:</h4>
+				<label for="key">Search word here:</label>
 				<input type="text" name="key" id="id_key" />
 				<input type="submit" />
 			</form>
