330928314beaef631806628706bdde8369cf9529
HsienTsung Wu <hsientsungwu@HsienTsungs-MacBook-Pro.local>
2013-04-05 22:47:45 -0400

    javascripts for search and autocomplete

--- js/application.js
+++ js/application.js
@@ -1,3 +1,34 @@
 $(document).ready(function(){
-	$('#id_key').autocomplete({source:'search2.php', minLength:2});
+	$('#id_key').autocomplete({
+		source: '/ajax/chtoeng.autocomplete.php', 
+		minLength: 2
+	});
+
+	$('#id_searchform').submit(function (e) {
+		e.preventDefault();
+
+		var key = $('#id_key').val();
+
+		if (key != '') {
+			$.ajax({
+			  	type: "POST",
+			  	url: "/ajax/chtoeng.search.php",
+			  	data: "chtoeng=1&key="+key,
+			  	datatype: "HTML",
+			  	success: function(html) {
+			  		if (html) {
+			  			$('.result-message').html("");
+			  			$('.results').html(html);
+			  		} else {
+			  			$('.result-message').html("No result found");
+			  		}
+			  	},
+			  	error: function() {
+			  		$('.result-message').html("Unable to retrieve result from the server");
+			  	}
+			});
+		} else {
+			$('.result-message').html("Search key cannot be blank");
+		}
+	});
 });
