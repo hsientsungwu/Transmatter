dfff7634bb49c7b7487115aef7f8fb188c1bf2e5
HsienTsung Wu <hsientsungwu@HsienTsungs-MacBook-Pro.local>
2013-04-05 22:47:30 -0400

    search file

--- ajax/chtoeng.search.php
+++ ajax/chtoeng.search.php
@@ -1,44 +1,29 @@
 <?php
 
-// if the 'term' variable is not sent with the request, exit
-if ( !isset($_REQUEST['term']) )
-	exit;
- 
-// connect to the database server and select the appropriate database for use
-/*$dblink = mysql_connect('server', 'username', 'password') or die( mysql_error() );
-mysql_select_db('database_name');
-*/
- 
-// query the database table for zip codes that match 'term'
-//$forms = dbFetchRows("SELECT id, name FROM Form where name LIKE '%". mysql_real_escape_string($_REQUEST['term']) ."%' ORDER BY name ASC limit 0, 10");
-$results = array(
-	array(
-		'id' => 1,
-		'name' => 'Steve Wu'
-	),
-	array(
-		'id' => 2,
-		'name' => 'Jackie Chan'
-	),
-	array(
-		'id' => 3,
-		'name' => 'Jet Li'
-	),
-);
+if (!$_POST['chtoeng'] && $key != '') exit;
 
- 
-// loop through each zipcode returned and format the response for jQuery
-$data = array();
+require $_SERVER['DOCUMENT_ROOT'] . "/config.php";
 
-if (count($forms)) {
-	foreach ($results as $result) {
-		$data[] = array(
-			'label' => $result['id'] . ' - ' . $result['name'],
-			'value' => $result['name']
-		);
+
+$results = $db->select('ce', 'tc LIKE :key', array(':key' => $key));
+
+
+header("Content-Type: text/html; charset=utf-8");
+
+$resultString = '<label>' . $key . '</label>';
+
+if (count($results)) {
+	foreach ($results as $index => $result) {
+		$resultString .= '<p>';
+		$eng = json_decode(($results['eng']));
+
+		$resultString .= implode(', ', $eng);
+
+		$resultString .= '</p>';
 	}
+} else {
+	$resultString .= '<p>No result found.</p>';
 }
 
-// jQuery wants JSON data
-echo json_encode($data);
-flush();
+echo $resultString;
+die; 
